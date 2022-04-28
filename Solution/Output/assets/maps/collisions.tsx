ed75f0eb04e96f56620fdfbe59b086c2b0330bec
Oga29 <79082037+Oga29@users.noreply.github.com>
2022-04-28 02:26:01 +0200

    Add files via upload

---
+++ Solution/Output/assets/maps/collisions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="collisions" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="prop_collsions_v01.png" width="96" height="33"/>
+</tileset>
---
+++ Solution/Output/assets/maps/tileset.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="32" tileheight="32" tilecount="42" columns="6">
+ <image source="tileset.png" width="192" height="224"/>
+ <tile id="0">
+  <properties>
+   <property name="detectAssemble" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="detectAssemble" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="detectAssemble" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="detectAssemble" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="detectAssemble" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="detectAssemble" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="detectAssemble" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="detectAssemble" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="detectAssemble" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="detectAssemble" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="detectAssemble" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="detectAssemble" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="detectAssemble" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="detectAssemble" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="detectAssemble" type="int" value="5"/>
+  </properties>
+ </tile>
+</tileset>
